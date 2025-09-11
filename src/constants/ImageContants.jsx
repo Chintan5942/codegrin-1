caa1ac2614e0d8eb4b113393edda20727a58c53b
Devan <devanbhensdadiya123@gmail.com>
2025-09-11 11:25:23 +0530

    404 Added

--- src/constants/ImageContants.jsx
+++ src/constants/ImageContants.jsx
@@ -1,4 +1,5 @@
 export const IMAGE_ASSETS = {
-    LOGO : "/src/assets/images/logo/logo-light.png",
-    SCROLL_TOP : "/src/assets/images/logo/scroll_top.svg",
+    LOGO : "./src/assets/images/logo/logo-light.png",
+    SCROLL_TOP : "./src/assets/images/logo/scroll_top.svg",
+    NOT_FOUND: "./src/assets/404.gif"
 }
--- src/pages/NotFound.jsx
+++ src/pages/NotFound.jsx
@@ -1,11 +1,14 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import { IMAGE_ASSETS } from "../constants/ImageContants";
+import { ROUTES } from "../constants/RoutesContants";
 
 export default function NotFound() {
   return (
-    <section className="flex items-center justify-center h-screen">
-      <h2>404 — Not Found</h2>
-      <p><Link to="/">Go back home</Link></p>
+    <section className="flex  bg-white flex-col items-center h-screen">
+      <img src={IMAGE_ASSETS.NOT_FOUND} alt=""/>
+      <h2 className="text-3xl absolute top-1/2 left-1/2 transform -translate-x-1/2 translate-y-1/2 text-center text-black font-bold">404 — Not Found</h2>
+      <Link to={ROUTES.HOME} className=" text-primary font-medium">Go to Home</Link>
     </section>
   );
 }
