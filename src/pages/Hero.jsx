4bf085f04d4fdba98969612847cf8554436cbbb9
Devan <devanbhensdadiya123@gmail.com>
2025-09-11 17:12:46 +0530

    Hero Section Fix

--- src/pages/Hero.jsx
+++ src/pages/Hero.jsx
@@ -15,10 +15,10 @@ export default function Hero() {
           disableRotation={true}
         >
           <div className="absolute top-1/5 transform text-center">
-            <h1 className="text-3xl lg:text-7xl font-bold text-white">
+            <h1 className="text-xl lg:text-7xl font-bold text-white">
               Turning Ideas Into Impact
             </h1>
-            <h1 className="text-3xl lg:text-7xl mt-2 lg:mt-3 font-bold inline-block pb-5 bg-gradient-to-b from-white from-20% to-primary to-55% bg-clip-text text-transparent">
+            <h1 className="text-xl lg:text-7xl mt-2 lg:mt-3 font-bold inline-block pb-5 bg-gradient-to-b from-white from-20% to-primary to-55% bg-clip-text text-transparent">
               Through the Power of Code
             </h1>
           </div>
