f9b495d53f4ba151f9ec2efbfdb4d435b1c28e74
Devan <devanbhensdadiya123@gmail.com>
2025-09-12 14:56:12 +0530

    Orbit animation added

--- src/pages/Hero.jsx
+++ src/pages/Hero.jsx
@@ -1,35 +1,33 @@
+import HeadingTitle from "../components/HeadingTItle";
+import Orbit from "../components/Orbit";
 import Particles from "../components/Particles";
 import { IMAGE_ASSETS } from "../constants/ImageContants";
 
 export default function Hero() {
   return (
-    <section className="w-full h-auto blend-y-end ">
-      <div className="absolute w-full h-[130vh] top-0">
+    <section className="w-full h-auto blend-y-end overflow-x-clip">
+      <div className="absolute w-full h-[130vh] top-0 -z-2">
         <Particles
-            particleColors={["#ffffff", "#ffffff"]}
-            particleCount={2000}
-            particleSpread={20}
-            speed={0.05}
-            particleBaseSize={60}
-            moveParticlesOnHover={true}
-            alphaParticles={false}
-            disableRotation={true}
+          particleColors={["#ffffff", "#ffffff"]}
+          particleCount={2000}
+          particleSpread={20}
+          speed={0.05}
+          particleBaseSize={60}
+          moveParticlesOnHover={true}
+          alphaParticles={false}
+          disableRotation={true}
         />
       </div>
-      <div className="w-full lg:pt-60 md:pt-50 pt-50  text-center pointer-events-none">
-        <h1 className="text-2xl md:text-5xl lg:text-7xl font-bold text-white">
-          Turning Ideas Into Impact
-        </h1>
-        <h1 className="text-2xl md:text-5xl lg:text-7xl lg:mt-3 font-bold inline-block pb-5 bg-gradient-to-b from-white from-20% to-primary to-55% bg-clip-text text-transparent">
-          Through the Power of Code
-        </h1>
-      </div>
+  
+      <HeadingTitle word1={"Turning Ideas Into Impact"} word2={"Through the Power of Code"} className="lg:pt-50 md:pt-50 pt-50 pb-20"/>
 
+      <Orbit planetCount={10} radius={{ base: "40%", md: "35%", lg: "27%"}} duration={45000} className="translate-y-45 md:translate-y-65 lg:translate-y-115" />
       <div className="relative flex w-full justify-center items-center pointer-events-none">
+      
         <img
           src={IMAGE_ASSETS.HERO_SECTION_1}
           alt=""
-          className="absolute  w-3/4 md:w-2/3 lg:w-1/2  left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2 z-0 animate-spin drop-shadow-[0_0_20px_cyan]"
+          className="absolute  w-3/4 md:w-2/3 lg:w-1/2  left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2 z-0 animate-spin drop-shadow-[0_0_15px_cyan]  lg:drop-shadow-[0_0_20px_cyan]"
           style={{ animationDuration: "120s" }}
         />
 
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,19 +3,19 @@ import { Link } from "react-router-dom";
 import Particles from "../components/Particles";
 import { IMAGE_ASSETS } from "../constants/ImageContants";
 import Hero from "./Hero";
+import HeadingTitle from "../components/HeadingTItle";
+import Orbit from "../components/Orbit";
 
 export default function Home() {
   return (
     <div>
       {/* Hero */}
-      <Hero/>
+      <Hero />
 
-      {/* Container-based section */}
+      {/* Achivements */}
       <div className="container">
-        <section className="w-full h-screen  flex items-center justify-center">
-          <h2 className="text-white text-3xl font-semibold">
-            Container Section
-          </h2>
+        <section className="w-fullflex items-center justify-center">
+          <HeadingTitle word1={"Our"} word2={"Achivement"} direction="row"/>
         </section>
       </div>
     </div>
